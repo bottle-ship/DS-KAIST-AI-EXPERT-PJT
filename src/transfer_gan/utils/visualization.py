0ec91086a05045bc4a7b9855844d605097f644ab
Byungseon Choi <garveira85@gmail.com>
2019-08-13 16:39:42 +0900

    Update function for generated image visualization

--- src/transfer_gan/utils/visualization.py
+++ src/transfer_gan/utils/visualization.py
@@ -21,3 +21,29 @@ def show_image_from_dataset(x, y, class_names=None):
     plt.subplots_adjust(wspace=0.1, hspace=0.0)
     plt.show()
     plt.close()
+
+
+def show_generated_image(x, filename=None):
+    if x.shape[0] > 25:
+        x = x[:25]
+
+    img_channel = x.shape[-1]
+
+    fig = plt.figure(figsize=(5, 5))
+    ax = list()
+
+    for i in range(0, 25):
+        ax.append(fig.add_subplot(5, 5, i + 1))
+        if img_channel == 1:
+            ax[i].imshow(x[i, :, :, 0], cmap='gray')
+        else:
+            ax[i].imshow(x[i])
+        ax[i].axis("off")
+
+    plt.subplots_adjust(wspace=0.1, hspace=0.0)
+
+    if filename is not None:
+        plt.savefig(filename)
+    else:
+        plt.show()
+    plt.close()
