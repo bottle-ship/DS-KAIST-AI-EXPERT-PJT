4cfbd5679b8a3a53b3bbc13af0617b1a6a2d65c4
Byungseon Choi <garveira85@gmail.com>
2019-08-15 14:09:00 +0900

    Fix return bug list to string

--- src/transfer_gan/utils/os_utils.py
+++ src/transfer_gan/utils/os_utils.py
@@ -14,3 +14,6 @@ def make_directory(path, time_suffix=False):
         for i in range(0, len(path)):
             if not os.path.exists(os.sep.join(path[:i + 1])):
                 os.mkdir(os.sep.join(path[:i + 1]))
+        path = os.sep.join(path)
+
+        return path
