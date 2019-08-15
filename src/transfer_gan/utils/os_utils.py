e19d237bd9d82853ba212ece9ed47063714c239d
Byungseon Choi <garveira85@gmail.com>
2019-08-15 10:08:29 +0900

    Update make_directory

--- src/transfer_gan/utils/os_utils.py
+++ src/transfer_gan/utils/os_utils.py
@@ -1,7 +1,12 @@
 import os
 
+import datetime
 
-def make_directory(path):
+
+def make_directory(path, time_suffix=False):
+    if time_suffix:
+        path += datetime.datetime.now().strftime("_%Y%m%d-%H%M%S")
+
     if os.path.exists(path):
         raise FileExistsError("'%s' is already exists." % path)
     else:
