a4ac283b7b6b7d7ceb70dc94595d84aadbcbc60d
Byungseon Choi <garveira85@gmail.com>
2019-08-19 12:28:14 +0900

    Add pre compute FID stats

---
+++ src/transfer_gan/datasets/_fid_stats/fid_stats_tiny_imagenet.py
@@ -0,0 +1,9 @@
+from transfer_gan.datasets import DatasetLoader
+from transfer_gan.metrics.fid import create_realdata_stats
+
+
+if __name__ == '__main__':
+    loader = DatasetLoader()
+    (x_train, y_train), (_, _), class_names = loader.load_tiny_imagenet()
+
+    create_realdata_stats(x_train, './fid_stats_tiny_imagenet.npz')
